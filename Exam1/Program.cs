065f34397adbf9c2dd2bb05e352bfdee94a2f75f
Nikolay <labazopelastra@gmail.com>
2022-11-30 00:35:04 +0700

    Добавили решение первого задания

---
+++ Exam1/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program{
+ void kvadrat(int num){
+    int result=num*num;
+    Console.WriteLine(result);
+}
+
+public static void Main(string []args){
+Program p=new Program();
+p.kvadrat(3);
+}
+}
