52060a41961f1d33a213f8c6dd119bbffbebe373
Nikolay <labazopelastra@gmail.com>
2022-11-30 01:12:59 +0700

    add two Exam

---
+++ Exam2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int a=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b=int.Parse(Console.ReadLine()!);
+
+if(b*b==a){
+    Console.WriteLine("True");
+}
+else{
+    Console.WriteLine("False");
+}
+
