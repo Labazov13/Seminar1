0b09ae6556906bcb06a5618772fafdd999d5a60e
Nikolay <labazopelastra@gmail.com>
2022-11-30 02:15:57 +0700

    add new cicle if

--- Exam5/Program.cs
+++ Exam5/Program.cs
@@ -1,6 +1,12 @@
 ﻿Console.WriteLine("Введите число");
 int num=int.Parse(Console.ReadLine()!);
+if(num >=100 &num<=999){
 Console.WriteLine(num%10);
+}
+else{
+    Console.WriteLine("Введите 3-значное число");
+}
+
 
 
 
